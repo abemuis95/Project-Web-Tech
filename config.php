982656bad3e21dc812a2983b18ce1ba6ece908e5
Amirul Naim <naimteehee@gmail.com>
2018-05-18 22:16:23 +0800

    Updated

--- config.php
+++ config.php
@@ -2,14 +2,17 @@
 
 	// EDIT HERE
 	// Server or Host name, Database Username, Database Password, Database Name goes in here
-	define ('hostnameorservername','localhost');
-	define ('serverusername','root');
-	define ('serverpassword','');
-	define ('databasenamed','db_project');
+	define ('hostname','localhost');
+	define ('username','root');
+	define ('password','');
+	define ('dbname','db_project');
 
 	// Make connection with database
 	global $connection;
-	$connection = @mysqli_connect(hostnameorservername,serverusername,serverpassword) or die('1. Connection could not be made to the SQL Server. Please report this system error to admin.');
-	@mysqli_select_db($connection, databasenamed) or die('2. Connection could not be made to the database. Please report this system error to admin.');	
+	$connection = mysqli_connect(hostname,username,password,dbname) or die('Connection could not be made to the SQL Server. Please report this system error to admin.');
+	
+	if(!$connection) {
+	    die('Could not connect: ' . mysqli_error($connection));
+	}	
 
 ?>
--- order.php
+++ order.php
@@ -2,12 +2,62 @@
 
 	include('config.php');
 
-	// SQL statement to update event table of status_data to 0 if the date has pass
-	$updatedata = "UPDATE event SET status_data = 0 WHERE start_date < '" . $currentdate . "' AND (end_date < '" . $currentdate . "' OR end_date is NULL)";
-	$query1 = mysqli_query($connection, $updatedata); // Execute the sql statement
+	if(empty($_REQUEST["customer"]))
+	    $customer = 'FAIL FETCH CUSTOMER';
+	else
+		$customer = (array) json_decode($_REQUEST["customer"]);
 
-	class SaveOrder() { . . . }
-	class ListOrder() { . . . }
-	class DeleteOrder() { . . . }
+	if(empty($_REQUEST["orderList"]))
+	    $orderList = 'FAIL FETCH ORDERLIST';
+	else
+		$orderList = (array) json_decode($_REQUEST["orderList"]);
+
+	if(empty($_REQUEST["function"]))
+	    $function = 'FAIL FETCH FUNCTION';
+	else
+		$function = $_REQUEST["function"];
+
+	switch ($function) {
+		case 'save':
+			SaveOrder($customer, $orderList, $connection);
+			break;
+
+		case 'list':
+			ListOrder();
+			break;
+
+		case 'delete':
+			DeleteOrder();
+			break;
+		
+		default:
+			# code...
+			break;
+	}
+
+
+	function SaveOrder($customer, $orderList, $connection) {
+		$insertdata = "INSERT INTO customers (name, address, area) VALUES ('" . $customer['name'] . "', '" . $customer['address'] . "', '" . $customer['area'] . "')";
+		$queryinsert = mysqli_query($connection, $insertdata) or die(mysqli_error($connection));
+
+		$id = $connection->insert_id;
+		if(count($orderList) > 0) {
+			$insertdata = "INSERT INTO orders (id_customer, item, price, qty) VALUES ('" . intval($id) . "',  '" . $orderList[0]->item . "', '" . floatval($orderList[0]->price) . "', '" . intval($orderList[0]->qty) . "')";
+
+			if(count($orderList) > 1)
+			for($i = 1; $i < sizeOf($orderList); $i++) {
+				$insertdata .= ", ('" . intval($id) . "',  '" . $orderList[$i]->item . "', '" . floatval($orderList[$i]->price) . "', '" . intval($orderList[$i]->qty) . "')";
+			}
+			$queryinsert = mysqli_query($connection, $insertdata) or die(mysqli_error($connection));
+		}
+	}
+
+	function ListOrder() {
+
+	}
+
+	function DeleteOrder() {
+		
+	}
 
 ?>
